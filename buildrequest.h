da7e35e2e23ac489425c5bec5aa1831798615ee7
muupu <once645@gmail.com>
2015-10-12 16:45:32 +0800

    add __BUILDREQUEST_H__

--- buildrequest.h
+++ buildrequest.h
@@ -1,5 +1,10 @@
+#ifndef __BUILDREQUEST_H__
+#define __BUILDREQUEST_H__
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 void build_request (const char *url);
+
+#endif /* __BUILDREQUEST_H__ */
